b311feba32007347e5e52a39ea0ca8de0a208fbe
psrs2000 <sanna.paulo@gmail.com>
2025-07-14 17:52:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3546,7 +3546,7 @@ Sistema de Agendamento Online
                                 if horarios_selecionados_semanal:
                                     if adicionar_bloqueio_semanal(dia_semana_selecionado, horarios_selecionados_semanal, descricao_semanal):
                                         st.success(f"✅ Bloqueio semanal para {dias_opcoes[dia_semana_selecionado]} criado com sucesso!")
-                                        st.rerun()
+                                        #st.rerun()
                                     else:
                                         st.warning("⚠️ Esse bloqueio semanal já existe ou ocorreu um erro.")
                                 else:
