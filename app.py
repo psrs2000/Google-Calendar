cc2ae51c357c56382c070c21d3cda7bed8822e69
psrs2000 <sanna.paulo@gmail.com>
2025-07-17 01:45:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1881,7 +1881,7 @@ def backup_agendamentos_futuros_github():
         
         # Buscar apenas agendamentos futuros
         c.execute("""
-            SELECT id, Data, Horário, Nome, Telefone, Email, Status, criado_em 
+            SELECT ID, Data, Horário, Nome, Telefone, Email, Status
             FROM agendamentos 
             WHERE Data >= ? 
             ORDER BY Data, Horário
